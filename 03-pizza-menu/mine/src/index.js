0af29f78e4a8d57aa77ec1788f0ae57eb0f38d81
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-08-19 16:04:01 +0700

    Commit 010 - Section 3 : Working with Components, Props, and JSX [Pizza Menu : CRA - Root - Strict Mode - Debugging - Components as Building Blocks - Reusing Components - JSX - More Components - JavaScript Logic in Components]

---
+++ 03-pizza-menu/mine/src/index.js
@@ -0,0 +1,111 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Footer />
+    </>
+  );
+}
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </>
+  );
+}
+
+function Footer() {
+  // Sans Babel, on écrirait comme ça
+  // return React.createElement("footer", null, "We're currently open");
+
+  // Réelle façon d'écrire (avec Babel)
+  <footer>{new Date().toLocaleDateString()}. We're currently open</footer>;
+}
+
+function Pizza() {
+  return (
+    <>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </>
+  );
+}
+
+// ------------------------------------------------------
+// ------------------------------------------------------
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"))
+// Penser à checker le fichier package.json pour vérifier la version de React
