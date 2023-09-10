3be6b4792e610ada89f4a5ac04bbb7bf7a488a14
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-09-10 20:04:18 +0700

    Commit 039 - Revision & Update Section 3 : Pizza Menu [Setting Classes and text Conditionally]

--- 03-pizza-menu/mine/src/index.js
+++ 03-pizza-menu/mine/src/index.js
@@ -136,12 +136,13 @@ function Footer() {
 
 function Pizza({ pizzaObject }) {
   console.log(pizzaObject);
+
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObject.soldOut ? `sold-out` : ""}`}>
       <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <h3>{pizzaObject.name}</h3>
       <p>{pizzaObject.ingredients}</p>
-      <span>{pizzaObject.price + 3}</span>
+      <span>{pizzaObject.soldOut ? `SOLD OUT` : pizzaObject.price}</span>
     </li>
   );
 }
