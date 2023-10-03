92f379bd730aad4719ae77ff2c0289bd7c074ea7
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-10-03 17:04:49 +0700

    Commit 077 - Section 7 : UsePopCorn [Props as a Component API -- Improving Reusability with Props part 1]

--- 07-usepopcorn/starter/usepopcorn/src/index.js
+++ 07-usepopcorn/starter/usepopcorn/src/index.js
@@ -8,6 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating />
+    <StarRating
+      maxRating={5}
+      defaultRating={4}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={20} size={24} color="red" className="test" />
   </React.StrictMode>
 );
