2fe28feea891a096dc475ad184315994b607b89a
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-10-07 18:28:37 +0700

    Commit 094 - Section before 9 : Go Back to UsePopCorn for Data Fetching [Adding a Loading State]

--- 07-usepopcorn/starter/usepopcorn/src/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/App.js
@@ -59,15 +59,18 @@ const key = `8047cb10`;
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "matrix";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${key}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
       // console.log(movies);
       // console.log(data.Search);
     }
@@ -82,9 +85,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -93,6 +94,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 // ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
